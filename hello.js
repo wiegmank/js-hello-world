bc4be65717018c4fadc4950255e7e564dd5dacb0
Kevin Wiegman <wiegmank@gmail.com>
2023-08-17 08:22:07 -0500

    <chpt 2 - '1st program'>

--- hello.js
+++ hello.js
@@ -4,7 +4,7 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("Hello");
+console.log('Quoth the Raven "Nevermore".' + " second message")
 
 /*  
 Next, PLAY! 
