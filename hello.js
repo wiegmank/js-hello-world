55283e24abe276e4d66e255efeb04358c1d8ff09
Kevin Wiegman <kevin@Kevins-MacBook-Air.local>
2023-09-05 09:08:05 -0500

    from new mac

--- hello.js
+++ hello.js
@@ -4,7 +4,7 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log('Quoth the Raven "Nevermore".' + " This is another message")
+console.log('Quoth the Raven "Nevermore".' + " This is another message!")
 
 /*  
 Next, PLAY! 
